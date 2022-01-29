8c0785cc91ff7d1718aceac6929d1f1e4153fa9e
FFRfree <free.sun@foxmail.com>
2022-01-30 00:05:22 +0800

    适配了移动端，修复bug

--- gpa/main.js
+++ gpa/main.js
@@ -1,4 +1,12 @@
 window.onload = function () {
+    function bindDeleteButton() {
+        $(".delete").click(function () {
+            // console.log(this);
+            $(this).parent().parent().remove()
+        })  
+    }
+    bindDeleteButton()
+
     function appendRow(num = 1,courseName='', weight='', score='') {
         for (let _ = 0; _ < num; _++) {
             let row = '<tr class="data">'+
@@ -13,10 +21,7 @@ window.onload = function () {
     }
     $("#appendRow").click(function () {
         appendRow(1);
-        $(".delete").click(function () {
-            // console.log(this);
-            $(this).parent().parent().remove()
-        })
+        bindDeleteButton();
     })
 
     $("#calculate").click(function () {
@@ -26,6 +31,7 @@ window.onload = function () {
             let credit = $(elem).find("td input[type='number']")[0].value
             let score = $(elem).find("td input[type='number']")[1].value
             let weighted_sum = credit * score
+            weighted_sum = Math.round(weighted_sum * 10000)/10000
             $(elem).find("td.weightedSum")[0].innerHTML = weighted_sum
         })
         let selected1 = $(".credit")
@@ -49,29 +55,51 @@ window.onload = function () {
         $("#averageOutput").text("你的平均GPA是: " + sum / creditSum)
     })
 
-    $(".delete").click(function () {
-        // console.log(this);
-        $(this).parent().parent().remove()
-    })
+    
 
+    function grade2centesimal(grade) { 
+        switch (grade) {
+            case "A":
+                return 4
+            case "B+":
+                return 3.5
+            case "B":
+                return 3
+            case "C+":
+                return 2.5
+            case "C":
+                return 2
+            case "D+":
+                return 1.5
+            case "D":
+                return 1
+            default:
+                return 1
+        }
+    }
     function processRawString(data) {
         data = data.split('\n')
         data = data.map(element => {
             let e = element.split('\t');
             let courseName = e[3];
             let weight = e[5];
-            let score = e[10];
             let grade = e[11];
-            return [courseName, weight, score, grade];
+            if(typeof grade === 'string'){
+                grade = grade2centesimal(grade)
+            }
+            return [courseName, weight, grade];
         });
         return data
     }
-
+    grade2centesimalMap = {
+        "A": 4,
+    }
     $("#import").click(function () {
         let rawString = prompt("将表格数据复制到此处");
         let desiredData = processRawString(rawString); //2d array
         for (row of desiredData){
             appendRow(num=1, courseName=row[0], weight=row[1], score=row[2])
         }
+        bindDeleteButton()
     })
 }
